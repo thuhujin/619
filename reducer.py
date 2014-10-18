e7d6efd1570476fa6c162c7b3fd64410ddc0819f
loono <linnanthu@163.com>
2014-10-18 11:10:22 -0400

    eliminate duplicate

--- reducer.py
+++ reducer.py
@@ -3,10 +3,14 @@
 import sys
 
 magic = '&efas&'
+old_id = None
 
 for line in sys.stdin:
 	line = line.replace('\n', '')
 	key, value= line.split('\t')
 	userid, date = key.split(magic)
 	textid, score, text = value.split(magic)
+	if textid == old_id:
+		continue
 	print '%s\t%s\t%s\t%s\t%s' % (userid, date, textid, score, text)
+	old_id = textid
