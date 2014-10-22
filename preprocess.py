8a68cc1d3c9930d50b6068c062daa069c041dd69
Jin Hu <thuhujin@gmail.com>
2014-10-22 14:42:39 -0400

    s

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,7 @@
 import os
 import sys
 def main():
-  numfile = 24
+  numfile = 2
   monthDict = {'Jan':'01', 'Feb':'02', 'Mar':'03', 'Apr':'04', 'May':'05', 'Jun':'06', 'Jul':'07', 'Aug':'08', 'Sep':'09', 'Oct':'10', 'Nov':'11', 'Dec':'12'}
   for i in range(numfile):
     if i < 10:
@@ -11,8 +11,8 @@ def main():
       filenamenum = "000" + str(i)
     inputfile = open("part-"+filenamenum,'r')
     outputfile = open("out-part-"+filenamenum,'w')
-    inputcontent = inputfile.read().splitlines()
-    for line in inputcontent:
+    line = inputfile.readline()
+    while line:
       if len(line.split('\t')) == 5:
         user_id = line.split('\t')[0]
         timestring = line.split('\t')[1]
@@ -22,5 +22,6 @@ def main():
         tweet_id = line.split('\t')[2]
         sentiment = line.split('\t')[3]
         tweet = line.split('\t')[4]
-        outputfile.write(user_id+':'+timestring+'\t'+tweet_id+'\t'+sentiment+'\t'+tweet+'\n')
+        outputfile.write(user_id+':'+timestring+'\t'+tweet_id+'\t'+sentiment+'\t'+tweet)
+      line = inputfile.readline()
 main()
